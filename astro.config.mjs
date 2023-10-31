f559689634e0f2b3a437b1993ff20ffeca2794f5
Nesb01t <nesb01t@163.com>
2023-10-31 13:16:44 +0800

    Updates astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://nesb01t.github.io',
+    base: 'my-repo'
 });
