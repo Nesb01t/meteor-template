0afc770577da7c98c0548f012e0b5c816d3f3773
nesb01t <nesb01t@163.com>
2024-07-17 22:28:51 +0800

    fix: style

--- src/components/hero-card.tsx
+++ src/components/hero-card.tsx
@@ -1,9 +1,5 @@
 import { Icon } from '@iconify-icon/solid';
 
-const navigateToResume = () => {
-  window.open(window.location.href + 'resume', '_blank');
-};
-
 const cardBackgroundStyle = {
   background: 'linear-gradient(90deg, #ededed81 0%, #edfbff99 100%)',
 };
@@ -11,8 +7,10 @@ const cardBackgroundStyle = {
 export interface Props {
   title: string;
   desc: string[];
+
   [key: string]: any;
 }
+
 export const HeroCard = (props: Props) => {
   return (
     <div
@@ -28,23 +26,14 @@ export const HeroCard = (props: Props) => {
         <div class="mb-1">{item}</div>
       ))}
       <div class="flex items-center gap-1">
-        我的简历入口
-        <a onClick={navigateToResume}>
-          <Icon icon="mingcute:open-door-line" />
+        <span>简历入口</span>
+        <a href="/resume">
+          <Icon
+            icon="mingcute:open-door-line"
+            class="text-[28px] flex items-center hover:text-purple-400 transition-all duration-300"
+          />
         </a>
       </div>
-      <Icon
-        class="absolute bottom-[-40px] right-[-40px] size-[180px]"
-        icon="line-md:cog-loop"
-      />
-      <Icon
-        class="absolute bottom-[70px] right-[135px] size-[110px]"
-        icon="line-md:compass-twotone-loop"
-      />
-      <Icon
-        class="absolute bottom-[30px] right-[270px] show-only-large-screen size-[80px]"
-        icon="line-md:moon-alt-to-sunny-outline-loop-transition"
-      />
     </div>
   );
 };
